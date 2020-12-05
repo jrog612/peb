ba20b7b00a5803a0fa2a703f7493897a36dd5d3b
joyongjin <wnrhd114@gmail.com>
2020-12-05 14:59:12 +0900

    add regex, utils, case and modify some function

--- peb/number.py
+++ peb/number.py
@@ -3,11 +3,11 @@ import decimal
 
 def half_round(num, fmt):
     """
+    decimal_round(0.1235, '1.0') == 0.0
     decimal_round(0.1235, '0.1') == 0.1
     decimal_round(0.1235, '0.01') == 0.12
     decimal_round(0.1235, '0.001') == 0.124
     decimal_round(0.1235, '0.0001') == 0.1235
-    decimal_round(0.12345, '1') == 0.0
     """
     if fmt.find('.') >= 0:
         # Float
@@ -15,7 +15,7 @@ def half_round(num, fmt):
         return float(n)
     else:
         # Integer
-        pass
+        raise ValueError('Format not allowed')
 
 
 def clear_int(num, clear_with):
@@ -28,3 +28,25 @@ def clear_int(num, clear_with):
 
 def intlen(num):
     return len(str(num))
+
+
+def num_to_percent(part, whole):
+    if whole == 0:
+        return 0
+    elif part > whole:
+        raise ValueError('part must be less than whole.')
+    return 100 * float(part) / float(whole)
+
+
+def percent_to_num(whole, percent):
+    if whole == 0:
+        return 0
+    return float(percent * whole) / 100
+
+
+def isint(some):
+    try:
+        int(some)
+        return True
+    except ValueError:
+        return False
