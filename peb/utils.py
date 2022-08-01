ec119a3631c7b9ca7ea0feb57c9984649285057a
jrog612 <wnrhd114@gmail.com>
2022-08-01 19:34:25 +0900

    safeaccess refactoring

--- peb/utils.py
+++ peb/utils.py
@@ -1,21 +1,27 @@
+from peb import isiter
+
+
 def raise_or(e, value, throw=False):
     if throw:
         raise e
     return value
 
 
-def safeaccess(obj, *args, context=None, default=None):
-    if obj is None:
+def safeaccess(obj, s, default=None):
+    if not obj or not s:
         return default
-
-    context = context or dict()
     value = obj
+    args = s.split('.')
 
-    for arg in args:
-        value = getattr(value, arg)
-        if value is None:
-            return default
-        if callable(value):
-            value = value(**context)
+    try:
+        for arg in args:
+            if type(value) is dict:
+                value = value[arg]
+            elif isiter(value, allow_dict=False):
+                value = value[int(arg)]
+            else:
+                return default
+    except (IndexError, KeyError, ValueError):
+        return default
 
     return value
